4a127f2cc46a72f7d683abcbe26c4847531b8666
hongjee <hongjee@yahoo.com>
2018-01-25 22:57:22 +0000

    Create SenderConfig.java

---
+++ kafkademo1/src/main/java/demo/kafka/producer/SenderConfig.java
@@ -0,0 +1,54 @@
+package demo.kafka.producer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+@Configuration
+public class SenderConfig {
+
+	@Value("${kafka.bootstrap-servers}")
+	private String bootstrapServers;
+
+	@Bean
+	public Map<String, Object> producerConfigs() {
+		// https://kafka.apache.org/0110/javadoc/org/apache/kafka/clients/producer/ProducerConfig.html
+		
+		Map<String, Object> props = new HashMap<>();
+		// a list of host:port pairs used for establishing the initial connections
+		// to the Kakfa cluster
+		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+		
+		// A message in Kafka is a key-value pair with a small amount of associated metadata. 
+		// As Kafka stores and transports Byte arrays, we need to specify the format from which 
+		// the key and value will be serialized. In this example we are sending a String as payload, 
+		// as such we specify the StringSerializer class which will take care of the needed transformation.
+		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+		return props;
+	}
+
+	@Bean
+	public ProducerFactory<String, String> producerFactory() {
+		return new DefaultKafkaProducerFactory<>(producerConfigs());
+	}
+
+	@Bean
+	public KafkaTemplate<String, String> kafkaTemplate() {
+		return new KafkaTemplate<>(producerFactory());
+	}
+
+	@Bean
+	public Sender sender() {
+		return new Sender();
+	}
+}
