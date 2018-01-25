f7d08893d80d8ba89fc80db4d2464374b14ec50a
hongjee <hongjee@yahoo.com>
2018-01-25 22:59:20 +0000

    Create ReceiverConfig.java

---
+++ kafkademo1/src/main/java/demo/kafka/consumer/ReceiverConfig.java
@@ -0,0 +1,64 @@
+package demo.kafka.consumer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+
+@Configuration
+// The  @EnableKafka  annotation which enables the detection of the  @KafkaListener  annotation that was used on the Receiver  class.
+@EnableKafka
+public class ReceiverConfig {
+
+	@Value("${kafka.bootstrap-servers}")
+	private String bootstrapServers;
+
+	@Bean
+	public Map<String, Object> consumerConfigs() {
+		// https://kafka.apache.org/0110/javadoc/index.html?org/apache/kafka/clients/consumer/ConsumerConfig.html
+		
+		Map<String, Object> props = new HashMap<>();
+		// a list of host:port pairs used for establishing the initial connections
+		// to the Kafka cluster
+		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+		
+		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		
+		// allows a pool of processes to divide the work of consuming and
+		// processing records
+		props.put(ConsumerConfig.GROUP_ID_CONFIG, "kafkademo1");
+		// automatically reset the offset to the earliest offset
+		props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+		return props;
+	}
+
+	@Bean
+	public ConsumerFactory<String, String> consumerFactory() {
+		return new DefaultKafkaConsumerFactory<>(consumerConfigs());
+	}
+
+	@Bean
+	public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, String>> kafkaListenerContainerFactory() {
+		ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+		factory.setConsumerFactory(consumerFactory());
+
+		return factory;
+	}
+
+	@Bean
+	public Receiver receiver() {
+		return new Receiver();
+	}
+}
