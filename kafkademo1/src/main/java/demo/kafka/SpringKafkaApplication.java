397b8a9c8750616d68269bb3d949cbe2ddb24f4b
hongjee <hongjee@yahoo.com>
2018-01-25 18:13:14 -0500

    remove hidden unicode text

--- kafkademo1/src/main/java/demo/kafka/SpringKafkaApplication.java
+++ kafkademo1/src/main/java/demo/kafka/SpringKafkaApplication.java
@@ -2,9 +2,9 @@ package demo.kafka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplicationpublic 
-class SpringKafkaApplication {
-  public static void main(String[] args) {    
-    SpringApplication.run(SpringKafkaApplication.class, args);  
+@SpringBootApplication
+public class SpringKafkaApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(SpringKafkaApplication.class, args);
   }
 }
