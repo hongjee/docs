aca349c61074a997db4f2e7bfb0425dd08d8b60e
hongjee <hongjee@yahoo.com>
2018-01-25 22:54:53 +0000

    Update SpringKafkaApplication.java

---
+++ kafkademo1/src/main/java/demo/kafka/SpringKafkaApplication.java
@@ -0,0 +1,10 @@
+package demo.kafka;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplicationpublic 
+class SpringKafkaApplication {
+  public static void main(String[] args) {    
+    SpringApplication.run(SpringKafkaApplication.class, args);  
+  }
+}
